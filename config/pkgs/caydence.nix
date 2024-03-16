1c61306e2fabd71f31784e7a36158f2b5310dc80
Josh Brown <joshcarlbrown@gmail.com>
2024-03-16 15:51:44 +1100

    fix sha

--- config/pkgs/caydence.nix
+++ config/pkgs/caydence.nix
@@ -1,16 +1,12 @@
 {pkgs}:
 pkgs.rustPlatform.buildRustPackage rec {
-  pname = "caydence";
-  version = "0.1.0";
-  sha = "026p64n3a3alw5fir0xnnb8h8bd53lqq9028r6685qsfh7fxb9n9";
-
   src = pkgs.fetchCrate {
-    crateName = pname;
-    version = version;
-    sha256 = sha;
+    crateName = "caydence";
+    version = "0.1.0";
+    sha256 = "026p64n3a3alw5fir0xnnb8h8bd53lqq9028r6685qsfh7fxb9n9";
   };
 
-  cargoSha256 = sha;
+  cargoSha256 = "sha256-6+kyb1zmNEDrVdh0gQJL5IyEwEUWWdtGtRzknBaETDs=";
 
   meta = with pkgs.lib; {
     description = "a wallpaper switching utility with pomodoro capabilities";

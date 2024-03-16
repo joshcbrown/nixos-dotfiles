f5d42cc423f15d1776865595024faa1f9b56b792
Josh Brown <joshcarlbrown@gmail.com>
2024-03-16 11:04:40 +1100

    more neovim updates

--- config/home/neovim.nix
+++ config/home/neovim.nix
@@ -1,10 +1,196 @@
-{ config, pkgs, ... }:
-
-let
+{
+  config,
+  pkgs,
+  ...
+}: let
   plugins = pkgs.vimPlugins;
   theme = config.colorScheme.palette;
 in {
-  programs.neovim = {
+  programs.nixvim = {
     enable = true;
+
+    globals.mapleader = " "; # Sets the leader key to space
+
+    colorschemes.tokyonight.enable = true;
+    options = {
+      clipboard = "unnamedplus";
+      number = true; # Show line numbers
+      relativenumber = true; # Show relative line numbers
+      softtabstop = 2;
+      smartindent = true;
+      wrap = false;
+      swapfile = false;
+      backup = false;
+      hlsearch = false;
+      incsearch = true;
+      termguicolors = true;
+      scrolloff = 8;
+      updatetime = 50;
+    };
+
+    plugins = {
+      barbecue.enable = true;
+      gitsigns.enable = true;
+      telescope = {
+        enable = true;
+        keymaps = {
+          "<leader>ff" = "find_files";
+          "<leader>lg" = "live_grep";
+        };
+      };
+      nvim-colorizer.enable = true;
+      nvim-autopairs.enable = true;
+      nix.enable = true;
+      comment-nvim.enable = true;
+      lualine = {
+        enable = true;
+      };
+      startup = {
+        enable = true;
+        theme = "dashboard";
+      };
+      lsp = {
+        enable = true;
+        servers = {
+          tsserver.enable = true;
+          lua-ls.enable = true;
+          bashls.enable = true;
+          rust-analyzer = {
+            enable = true;
+            installRustc = true;
+            installCargo = true;
+          };
+          nixd.enable = true;
+          html.enable = true;
+          ccls.enable = true;
+          cmake.enable = true;
+          csharp-ls.enable = true;
+          cssls.enable = true;
+          gopls.enable = true;
+          jsonls.enable = true;
+          pyright.enable = true;
+          tailwindcss.enable = true;
+        };
+      };
+      conform-nvim = {
+        enable = true;
+        formattersByFt = {
+          lua = ["stylua"];
+          nix = ["alejandra"];
+        };
+        formatOnSave = ''{async = true, lsp_fallback = "always"}'';
+      };
+      lsp-lines.enable = true;
+      treesitter = {
+        enable = true;
+        nixGrammars = true;
+      };
+      nvim-cmp = {
+        enable = true;
+        autoEnableSources = true;
+        sources = [
+          {name = "nvim_lsp";}
+          {name = "path";}
+          {name = "buffer";}
+        ];
+        mapping = {
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<Tab>" = {
+            action = ''cmp.mapping.select_next_item()'';
+            modes = ["i" "s"];
+          };
+        };
+      };
+    };
+
+    extraPlugins = [plugins.telescope-file-browser-nvim];
+
+    # FOR NEOVIDE
+    extraConfigLua = ''
+      vim.opt.guifont = "JetBrainsMono\\ NFM,Noto_Color_Emoji:h14"
+      vim.g.neovide_cursor_animation_length = 0.05
+
+      local colors = {
+        blue   = '#${theme.base0D}',
+        cyan   = '#${theme.base0C}',
+        black  = '#${theme.base00}',
+        white  = '#${theme.base05}',
+        red    = '#${theme.base08}',
+        violet = '#${theme.base0E}',
+        grey   = '#${theme.base02}',
+      }
+
+      local bubbles_theme = {
+        normal = {
+          a = { fg = colors.black, bg = colors.violet },
+          b = { fg = colors.white, bg = colors.grey },
+          c = { fg = colors.black, bg = colors.black },
+        },
+
+        insert = { a = { fg = colors.black, bg = colors.blue } },
+        visual = { a = { fg = colors.black, bg = colors.cyan } },
+        replace = { a = { fg = colors.black, bg = colors.red } },
+
+        inactive = {
+          a = { fg = colors.white, bg = colors.black },
+          b = { fg = colors.white, bg = colors.black },
+          c = { fg = colors.black, bg = colors.black },
+        },
+      }
+
+      require('lualine').setup {
+        options = {
+          theme = bubbles_theme,
+          component_separators = '|',
+          section_separators = { left = '', right = '' },
+        },
+        sections = {
+          lualine_a = {
+            { 'mode', separator = { left = '' }, right_padding = 2 },
+          },
+          lualine_b = { 'filename', 'branch' },
+          lualine_c = { 'fileformat' },
+          lualine_x = {},
+          lualine_y = { 'filetype', 'progress' },
+          lualine_z = {
+            { 'location', separator = { right = '' }, left_padding = 2 },
+          },
+        },
+        inactive_sections = {
+          lualine_a = { 'filename' },
+          lualine_b = {},
+          lualine_c = {},
+          lualine_x = {},
+          lualine_y = {},
+          lualine_z = { 'location' },
+        },
+        tabline = {},
+        extensions = {},
+      }
+    '';
+
+    extraConfigVim = ''
+      set noshowmode
+      inoremap jj <ESC>
+    '';
+
+    keymaps = [
+      {
+        mode = "n";
+        key = "<space>fb";
+        action = ":Telescope file_browser<CR>";
+        options.noremap = true;
+      }
+      {
+        key = "<Tab>";
+        action = ":bnext<CR>";
+        options.silent = false;
+      }
+      {
+        key = "<S-Tab>";
+        action = ":bprev<CR>";
+        options.silent = false;
+      }
+    ];
   };
- }
+}

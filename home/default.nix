2ebd21596f68c0b226000953b43c1d980e67c76c
joshcbrown <joshcarlbrown@gmail.com>
2025-06-13 15:26:21 +1000

    remove firefox

--- home/default.nix
+++ home/default.nix
@@ -1,6 +1,5 @@
 {
-  imports =
-    [ ./term ./packages.nix ./files.nix ./hyprland ./anyrun ./firefox.nix ];
+  imports = [ ./term ./packages.nix ./files.nix ./hyprland ./anyrun ];
   home.username = "josh";
   home.homeDirectory = "/home/josh";
   home.stateVersion = "25.11";

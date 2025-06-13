db013402cc8405491c66251180a0acd4c2afc413
joshcbrown <joshcarlbrown@gmail.com>
2025-06-13 15:19:33 +1000

    firefox

--- home/default.nix
+++ home/default.nix
@@ -1,5 +1,6 @@
 {
-  imports = [ ./term ./packages.nix ./files.nix ./hyprland ];
+  imports =
+    [ ./term ./packages.nix ./files.nix ./hyprland ./anyrun ./firefox.nix ];
   home.username = "josh";
   home.homeDirectory = "/home/josh";
   home.stateVersion = "25.11";
